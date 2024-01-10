c4532004f0274bea1e30b2c5d1f0ac8462c5cb7f
Petar Dobrev <peter.dobrev@gmail.com>
2024-01-10 18:33:08 +0200

    Initial commit with a template project

---
+++ Pipeable/Pipeable.h
@@ -0,0 +1,18 @@
+//
+//  Pipeable.h
+//  Pipeable
+//
+//  Created by Petar Dobrev on 1/10/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Pipeable.
+FOUNDATION_EXPORT double PipeableVersionNumber;
+
+//! Project version string for Pipeable.
+FOUNDATION_EXPORT const unsigned char PipeableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Pipeable/PublicHeader.h>
+
+
