0f4760ca018344ec31e64022e8cfab4e95ab78f6
Petar Dobrev <peter.dobrev@gmail.com>
2024-01-16 17:33:12 +0200

    Added swiftlint as part of the CI

--- PipeableTests/PipeableTests.swift
+++ PipeableTests/PipeableTests.swift
@@ -2,7 +2,6 @@
 //  PipeableTests.swift
 //  PipeableTests
 
-
 import XCTest
 @testable import Pipeable
 
@@ -21,7 +20,8 @@ final class PipeableTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        // Mark your test async to allow awaiting for asynchronous code to complete. 
+        // Check the results with assertions afterwards.
     }
 
     func testPerformanceExample() throws {
