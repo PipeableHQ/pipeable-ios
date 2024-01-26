d81e56eb2848ac58e7b64a8da54aa6b46d9b52bc
Petar Dobrev <peter.dobrev@gmail.com>
2024-01-26 16:35:17 +0200

    Added a podspec to publish to cocoapods

--- Sources/Pipeable.swift
+++ Sources/Pipeable.swift
@@ -150,9 +150,6 @@ public class PipeablePage {
         var contents: String?
 
         let frameworkBundle = Bundle(for: PipeablePage.self)
-
-        let what = frameworkBundle.url(forResource: "sophia", withExtension: "js")
-
         let pathInFramework = frameworkBundle.path(forResource: "sophia", ofType: "js")
 
         if let filepath = pathInFramework {
