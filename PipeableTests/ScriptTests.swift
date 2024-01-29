76dd48c5d18860184a50252a5b5a34c89f8139cd
Ivaylo Bratoev <ivaylo.bratoev@gmail.com>
2024-01-29 18:42:33 +0200

    Add page.querySelector(...).click() to the Script PoC (#12)
    * Add page.querySelector(...).click() to the Script PoC
    
    Add a more complex use case for the Script mock that returns a native object (PipeableElement).
    
    During the implementation, found a way to properly use the wrappers prototypes. Refactored the code to add JS functions using the PageWrapper.prototype.
    
    Improve how we wait for all async operations to finish. Add a wait for the whole script as well to avoid race conditions.

--- PipeableTests/ScriptTests.swift
+++ PipeableTests/ScriptTests.swift
@@ -22,7 +22,35 @@ final class ScriptTests: XCTestCase {
             _ = try await runScript(script)
             XCTFail("An empty url error is expected")
         } catch let ScriptError.error(reason) {
-            XCTAssertEqual(reason, "Empty url parameter")
+            XCTAssertEqual(reason, "Bad url.")
+        } catch {
+            XCTFail("An empty url error is expected")
+        }
+    }
+
+    func test_querySelector_and_click() async throws {
+        let script = """
+            const element = await page.querySelector("asd");
+            await element.click();
+            return element;
+        """
+        let result = try await runScript(script)
+        // swiftlint:disable:next force_cast
+        let element = result.toObjectOf(PipeableElementWrapper.self) as! PipeableElementWrapper
+        XCTAssertEqual(element.successFullClicksClount, 1)
+    }
+
+    func test_querySelector_and_click_with_error() async throws {
+        let script = """
+            const element = await page.querySelector("empty");
+            await element.click();
+            return element;
+        """
+        do {
+            _ = try await runScript(script)
+            XCTFail("Element not found error is expected")
+        } catch let ScriptError.error(reason) {
+            XCTAssertEqual(reason, "Element not found.")
         } catch {
             XCTFail("An empty url error is expected")
         }
