99d7b33fb2408be8a042465da651b2ee7470c08a
Petar Dobrev <peter.dobrev@gmail.com>
2024-02-18 23:19:44 +0200

    hm, bumped up the timeouts

--- PipeableTests/PipeablePage_GotoTests.swift
+++ PipeableTests/PipeablePage_GotoTests.swift
@@ -83,7 +83,7 @@ final class PipeablePageGotoTests: PipeableXCTestCase {
     func testGotoDomcontentLoadedButNotLoad() async throws {
         let page = PipeablePage(webView)
 
-        try await page.goto("http://localhost:3000/load_latency/2/index.html", waitUntil: .domcontentloaded, timeout: 1000)
+        try await page.goto("http://localhost:3000/load_latency/5/index.html", waitUntil: .domcontentloaded, timeout: 3000)
 
         do {
             try await page.waitForLoadState(waitUntil: .load, timeout: 1000)
